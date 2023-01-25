869ec7d4b321622f632f46e56c672dfceecd2d56
mikelaslam <kipkorir385@gmail.com>
2023-01-25 15:51:55 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, {useState}from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+
 
 function App() {
+  const [categories, setCategories] = useState(CATEGORIES);
+
+  const [task, setTask] = useState(TASKS);
+
+  const [selectedCatBtn, setSelectedCatBtn] = useState("ALL");
+
+  const displayedItem = task.filter((item) => {
+    if (selectedCatBtn === "All") return true;
+    return selectedCatBtn === item.category;
+  });
+
+  const addNewItem = (newItem) => setTask([...task, newItem]);
+
+  const deleteButton = (deleted) =>
+    setTask(task.filter((item) => item.text !== deleted));
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={categories}
+        selectedBtn={setSelectedCatBtn}
+        onBtn={selectedCatBtn}
+      />
+
+      <NewTaskForm categories={categories} onTaskFormSubmit={addNewItem} />
+
+      <TaskList tasks={displayedItem} deletedItem={deleteButton} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,25 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter(props) {
+  const { selectedBtn, onBtn, categories } = props;
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+
+      {categories.map((category, i) => (
+        <button
+          className={onBtn === category ? "selected" : ""}
+          key={i}
+          onClick={() => {
+            selectedBtn(category);
+          }}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
 
 export default CategoryFilter;
+
